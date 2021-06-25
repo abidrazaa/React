75ca26e43584cfc1ea7c9440d5026cc6808a1627
Abid Raza <abid.neduet@gmail.com>
2021-06-26 01:24:15 +0500

    Props

---
+++ Week 1/class01/src/props.js
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+import CustomButton from './components/CustomButton';
+import Dashboard from './views/Dashboard';
+import Home from './views/Home';
+
+
+
+
+function App() {
+
+
+  const [screen, setScreen] = useState('Home')
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <CustomButton title="Home"/>
+        <CustomButton title="Dashboard"/>
+
+        {screen === "Home" &&
+        <Home />}
+        {screen === "Dashboard" &&
+        <Dashboard />}
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
