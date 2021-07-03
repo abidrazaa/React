a644299a8d58106519e163e63821fbbe743667ea
Abid Raza <abid.neduet@gmail.com>
2021-07-04 01:49:17 +0500

    Firebase

--- Week 1/class01/src/App.js
+++ Week 1/class01/src/App.js
@@ -3,77 +3,37 @@ import reactDom from 'react-dom';
 import './App.css'
 import PostCard from "./components/PostCard"
 import FbImageLibrary from 'react-fb-image-grid'
+import FbPost from './components/FbPost';
+import {register} from "./Config/firebase"
 
 
 function App() {
 
-
-  
+  var d = new Date();
+  var time = d.getTime();
+  var year = d.getFullYear();
+  var date = d.getDate();
+  var month = d.getMonth();
+  var compoleteDate = date + "/" + month + "/" + year 
+  const user = {
+    name: "Abid Raza",
+    image: "https://reactjs.org/logo-og.png",
+    message: "This is my first post",
+    time: time,
+    date: compoleteDate
+  }
 
 
   return (
     <div className="App" >
       <header className="App-header">
 
-        <h1>Facebook Single Post</h1>
-        <div className="post-card"
-          style= {{backgroundColor:"white", padding:20 }}
-        
-        >
-          <div
-          style={{display:"flex"}}
-          >
-            {/* <img src= alt="" /> */}
-            <div>
-              <img src="https://reactjs.org/logo-og.png" alt="image" 
-                style= {{height:40, width:40 , borderRadius:500}}
-                />
-                
-            </div>
-            <div
-              style= {{color:"black", fontSize:15, fontWeight:700}}
-            
-            >
-                &nbsp; Abid Raza
-                <br />
-                Date
-              </div>
-          </div>
-          <div className="images">
-            <FbImageLibrary images={["https://reactjs.org/logo-og.png","https://reactjs.org/logo-og.png","https://reactjs.org/logo-og.png","https://reactjs.org/logo-og.png","https://reactjs.org/logo-og.png","https://reactjs.org/logo-og.png"]}/>
-            
-          </div>
-
-          <div className="comments"
-          style={{display:"flex"}}       
-          >
-            <div>
-                <img src="https://reactjs.org/logo-og.png" alt="image" 
-                  style= {{height:40, width:40 , borderRadius:500}}
-                />
-                  
-              </div>
-              <div>
-              &nbsp;
-                  <input type="text" 
-                  style= {{borderRadius:300 , borderBlockColor:"black" }}
-                  
-                  />
-              </div>
-              <div>
-              &nbsp;
-                <button
-                style= {{ color:"white", backgroundColor:"green", fontSize:20 }}
-                
-                >post</button>
-              </div>
-
-          </div>
-        </div>
-       
-
-       
-
+        <h1>Facebook Single Post</h1> 
+        <FbPost user = {user} />
+        <input type="text" name="email" /> Email
+        <input type="text" name="password" /> Password
+        <button onClick={register}>Register</button>
+        <button onClick={login}>login</button>
 
       
 
