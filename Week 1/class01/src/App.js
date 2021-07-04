fece9b7378be104d63cee0445393845f38b36ce0
Abid Raza <abid.neduet@gmail.com>
2021-07-04 16:13:03 +0500

    firebase

--- Week 1/class01/src/App.js
+++ Week 1/class01/src/App.js
@@ -4,46 +4,97 @@ import './App.css'
 import PostCard from "./components/PostCard"
 import FbImageLibrary from 'react-fb-image-grid'
 import FbPost from './components/FbPost';
-import {register} from "./Config/firebase"
+import {register , login , getAllUsers} from "./Config/firebase"
+import Dashboard from "./components/Dashboard"
+import firebase from "firebase"
+
+
 
 
 function App() {
 
-  var d = new Date();
-  var time = d.getTime();
-  var year = d.getFullYear();
-  var date = d.getDate();
-  var month = d.getMonth();
-  var compoleteDate = date + "/" + month + "/" + year 
-  const user = {
-    name: "Abid Raza",
-    image: "https://reactjs.org/logo-og.png",
-    message: "This is my first post",
-    time: time,
-    date: compoleteDate
+  const db = firebase.firestore()
+
+  const [name,setName] = useState("")
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+  const [userLoggedIn, setUserLoggedIn ] = useState(false)
+  
+
+  const signIn = async() => {
+    try{
+      const userLoggedIn = await login(email,password)
+      setUserLoggedIn(true)
+      alert("User logged in")
+    }
+    catch(e){
+      alert(e.message)
+    }
   }
 
+  const signUp = async () => {
+    try{
+      const userSignUp = await register(email,password)
+      console.log("User signed Up ==> ",userSignUp.user.uid)
+      alert("User Created")
+      db.collection("users").doc(userSignUp.user.uid).set({name,email})
+    }
+    catch(e){
+      alert(e.message)
+    }
+  }
 
+  
+
+
+ 
   return (
     <div className="App" >
       <header className="App-header">
 
-        <h1>Facebook Single Post</h1> 
-        <FbPost user = {user} />
-        <input type="text" name="email" /> Email
-        <input type="text" name="password" /> Password
-        <button onClick={register}>Register</button>
-        <button onClick={login}>login</button>
+        {!userLoggedIn 
+        ?
+        <div>
+          <input type="text" placeholder="name" name="name" 
+          onChange={(e)=>{
+            setName(e.target.value)
+          }}
+          />
+          <input type="email" placeholder="email" name="email" 
+          onChange={(e)=>{
+            setEmail(e.target.value)
+          }}
+          />
+          <input type="password" placeholder="password" name="password" 
+          onChange={(e)=>{
+            setPassword(e.target.value)
+          }}
+          />
+          <button
+          onClick = {signIn}
+          >Login</button>
+          <button
+          onClick = {signUp}
+          >Register</button>
+          </div>
 
+          :
+
+          <Dashboard />
+
+        
       
+        }
+        
+        
+
+
+
 
       </header>
-    
-    
-    
     </div>
-  );
-      }
+       )     
+}
 
 export default App;
 
