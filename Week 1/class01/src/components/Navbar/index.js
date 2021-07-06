508dcb1c63c7e98784e7296c67181587695cfb9c
Abid Raza <abid.neduet@gmail.com>
2021-07-07 02:08:02 +0500

    Olx Navbar

---
+++ Week 1/class01/src/components/Navbar/index.js
@@ -0,0 +1,67 @@
+import { Navbar, NavDropdown, Nav, Button, FormControl, Form } from "react-bootstrap";
+
+function OlxNavbar() {
+    return (
+        <div>
+            <Navbar fixed="top" bg="light" expand="lg" 
+            style={{padding:10}}
+            >
+                <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                <Navbar.Toggle  />
+                <Navbar.Collapse>
+                    <Nav
+                        className="mr-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                    >
+                        <Nav.Link href="#action1">Home</Nav.Link>
+                        <Nav.Link href="#action2">Link</Nav.Link>
+                        <NavDropdown title="Link" >
+                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#" disabled>
+                            Link
+                        </Nav.Link>
+                    </Nav>
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    <Form className="d-flex">
+                        <FormControl
+                            type="search"
+                            placeholder="Search"
+                            className="mr-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    )
+}
+
+export default OlxNavbar;
