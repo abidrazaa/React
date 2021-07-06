03e67bc72d00d52c13cdd036524d80aed2560b6f
Abid Raza <abid.neduet@gmail.com>
2021-07-07 02:05:20 +0500

    Olx project

--- OLX/olx/src/components/Cards/index.js
+++ OLX/olx/src/components/Cards/index.js
@@ -1,22 +1,14 @@
 
+function OlxCard () {
 
-function card () {
+return(
+  <div className = "olx-card">
+    <h1>Wah</h1>
 
-return <>
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-</>
 
+  </div>
+)
 
 }
 
-export d
+export default OlxCard();
