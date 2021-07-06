764245e6da6f4f50cf3486a9f7d9ebb21053398f
Abid Raza <abid.neduet@gmail.com>
2021-07-07 02:03:26 +0500

    Olx project

--- OLX/olx/src/App.js
+++ OLX/olx/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import OlxCard from "./components/Cards"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      {/* <OlxCard /> */}
+      <h1> HII </h1>
+      <OlxCard />
+
         
       </header>
     </div>
