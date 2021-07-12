8dabaa4d1a2f979ef134ced2c17635b101ff3dd1
Abid Raza <abid.neduet@gmail.com>
2021-07-13 02:32:15 +0500

    olx

--- Week 1/class01/src/App.js
+++ Week 1/class01/src/App.js
@@ -4,6 +4,7 @@ import './App.css'
 import OlxCard from "./components/Card"
 import {Button} from "react-bootstrap";
 import OlxNavbar from "./components/Navbar"
+import Footer from './components/Footer';
 
 
 
@@ -15,8 +16,21 @@ function App() {
     <div className="App" >
       <header className="App-header">
         <OlxNavbar />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
 
         <OlxCard />
+        <OlxCard />
+        <OlxCard />
+
+        <br />
+        <br />
+        <br />
+
+        <Footer />
 
 
         {/* <Button>Click me</Button> */}
